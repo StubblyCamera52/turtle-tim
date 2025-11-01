7d16959a8eec664f3f211de516e1c06fb537a561
intenstudios <intenstudios369@gmail.com>
2025-11-01 11:54:35 -0700

    fic

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,14 +1,22 @@
-using Godot;
 using System;
+
+namespace TurtleTim.scripts;
+using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	private const float Speed = 1.0f;
+	private Vector2 _facingDirection;
+	private Label _debug;
 
   public override void _Ready()
-	{
+  {
+	  _debug = GetNode<Label>(new NodePath("Debug"));
 		base._Ready();
+		// facing right
+		_facingDirection = new Vector2(1, 0);
+		_debug.Text = "Ready";
+	  
   }
 
 	public override void _PhysicsProcess(double delta)
@@ -16,18 +24,22 @@ public partial class Player : CharacterBody2D
 		Vector2 velocity = Velocity;
 
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		if (Input.IsActionJustPressed("ui_dash"))
 		{
+			direction = direction.Normalized();
+			direction = new Vector2(direction.X + _facingDirection.X + 1, direction.Y + _facingDirection.Y);
+		}
+		if (direction != Vector2.Zero) {
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
 		}
-		else
-		{
+		else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 
-		Velocity = velocity;
+		_debug.Text = $"Veldocity: {velocity}\nDirection: {direction}\nFacing: {_facingDirection}";
+        Velocity = velocity;
 		MoveAndSlide();
 	}
 }
